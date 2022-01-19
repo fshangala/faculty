8c36c97cb008125e7d23838b1607d0f29ce7d3cd
Funduluka Shangala <fshangala@gmail.com>
2022-01-19 23:54:49 +0200

    added some content

--- src/FacultyServiceProvider.php
+++ src/FacultyServiceProvider.php
@@ -1,2 +1,11 @@
 <?php
 namespace Fshangala\Faculty;
+use Illuminate\Support\ServiceProvider;
+
+class FacultyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
